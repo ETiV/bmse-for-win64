306d06e8b3a11ae53ea46dfa3b4e58389c475b03
ETiV <ETiV@users.noreply.github.com>
2014-08-08 20:00:35 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,5 +6,7 @@ copy .\mscomctl.ocx %SystemRoot%\SysWOW64\
 copy .\dx7vb.dll %SystemRoot%\SysWOW64\
 copy .\dx8vb.dll %SystemRoot%\SysWOW64\
 
+regsvr32 %SystemRoot%\SysWOW64\comdlg32.ocx
+regsvr32 %SystemRoot%\SysWOW64\mscomctl.ocx
 regsvr32 %SystemRoot%\SysWOW64\dx7vb.dll
 regsvr32 %SystemRoot%\SysWOW64\dx8vb.dll
